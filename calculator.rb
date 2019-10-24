4da4fb68067973fad9bc3e66020a652b5d010538
amir shariff <amirshariff@msn.com>
2019-10-24 02:41:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_number = 10
+first_number = 30
 second_number = 15
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-qoutient = first_number difference second_number
+qoutient = first_number / second_number
